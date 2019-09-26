d4327e208338ce230b432ae05daf9e042e01cb68
Óscar Jiménez Fernández <44193393+yoskitar@users.noreply.github.com>
2019-09-26 19:15:09 +0200

    Update II_Poblacion_China.cpp

--- Sesion 5/II_Poblacion_China.cpp
+++ Sesion 5/II_Poblacion_China.cpp
@@ -1,25 +1,25 @@
 /*********************************************************************/
-// FUNDAMENTOS DE PROGRAMACI”N.
-// GRADO EN INGENIERÕA INFORM¡TICA.
+// FUNDAMENTOS DE PROGRAMACI√ìN.
+// GRADO EN INGENIER√çA INFORM√ÅTICA.
 //
 // CURSO 2015-2016
-// (C) ”scar JimÈnez Fern·ndez
+// (C) √ìscar Jim√©nez Fern√°ndez
 //
 // 
 /*    
-       SesiÛn 01-05.
-       Problema n˙mero 20:
-           Calcular n˙mero de aÒos que han de pasar hasta alcanzar
-           una poblaciÛn introducida desde teclado, mediante ciclos
-           while y filtros para controlar que la poblaciÛn ha alcanzar
+       Sesi√≥n 01-05.
+       Problema n√∫mero 20:
+           Calcular n√∫mero de a√±os que han de pasar hasta alcanzar
+           una poblaci√≥n introducida desde teclado, mediante ciclos
+           while y filtros para controlar que la poblaci√≥n a alcanzar
            sea mayor que la actual.
 			      
 */  
 /*********************************************************************/
 
 
-#include <iostream>              //InclusiÛn de recursos de E/S
-#include <cstdlib>              // InclusiÛn de llamadas al sistema.
+#include <iostream>              //Inclusi√≥n de recursos de E/S
+#include <cstdlib>              // Inclusi√≥n de llamadas al sistema.
 
 
 using namespace std;
@@ -27,24 +27,24 @@ using namespace std;
 int main(){                    //Programa principal.
             
 	               
-// DeclaraciÛn de variables para guardar.
+// Declaraci√≥n de variables para guardar.
 		   
    const int POBLAC_ACTUAL = 1375570814;       //Constantes para
-	const int YEARS = 1;                       //modificar su valor m·s
+	const int YEARS = 1;                       //modificar su valor m√°s
 	const double NACIM_S = 1.87;              //facilmente si fuese necesario.
 	const double MUERT_S = 3.27;              
    const double EMIGRAC_S = 71.9;
    
    
-	double nacimientos;            //Variables para calcular el n˙mero
+	double nacimientos;            //Variables para calcular el n√∫mero
 	double muertes;               //de nacimientos, muertes, emigraciones.
 	double emigraciones;
-   double tiempo_year_seg;         //Variable para conversiÛn de aÒos a seg.
+   double tiempo_year_seg;         //Variable para conversi√≥n de a√±os a seg.
 	
-	int poblacion_total;         //PoblaciÛn tras cada aÒo.
-	int anios;                   //AÒos hasta alcanzar la poblaciÛn pedida.
-	int poblacion_futura;        //PoblaciÛn ha alcanzar.
-	int poblacion_variada;       //PoblaciÛn total acumulada cada aÒo.
+	int poblacion_total;         //Poblaci√≥n tras cada a√±o.
+	int anios;                   //A√±os hasta alcanzar la poblaci√≥n pedida.
+	int poblacion_futura;        //Poblaci√≥n ha alcanzar.
+	int poblacion_variada;       //Poblaci√≥n total acumulada cada a√±o.
 	            	
 	
 	//Entrada de datos.
@@ -53,29 +53,29 @@ int main(){                    //Programa principal.
 	
 	do{
 		
-	cout << "\nIntroduzca la poblaciÛn para hallar cuantos aÒos"
-	<< " han de pasar hasta que se alcanze dicha poblaciÛn: ";  
+	cout << "\nIntroduzca la poblaci√≥n para hallar cuantos a√±os"
+	<< " han de pasar hasta que se alcanze dicha poblaci√≥n: ";  
 	cin >> poblacion_futura;
 	
    }while( poblacion_futura <= POBLAC_ACTUAL );
 	
-	//C·lculos.
+	//C√°lculos.
 	
-	//Factor de ConversiÛn (aÒos a segundos)	
+	//Factor de Conversi√≥n (a√±os a segundos)	
    tiempo_year_seg = YEARS*365*24*60*60;    
 
-   /* C·lculo del n˙mero de nacimientos, muertes y emigraciones en funciÛn  
-   de la variable (A—OS(1)). */
+   /* C√°lculo del n√∫mero de nacimientos, muertes y emigraciones en funci√≥n  
+   de la variable (A√ëOS(1)). */
    nacimientos = tiempo_year_seg/NACIM_S;           
    muertes = tiempo_year_seg/MUERT_S;             
    emigraciones = tiempo_year_seg/EMIGRAC_S; 
 	
 	
 	/*
-	Creamos una variable acumuladora que sumar· 1 aÒo cada vez
-	que el total de poblaciÛn sea inferior al doble de la cantidad
-   inicial de poblaciÛn, siendo asÌ el n˙mero de aÒos que deben pasar
-   para doblar dicha poblaciÛn el n˙mero de veces que se repite
+	Creamos una variable acumuladora que sumar√° 1 a√±o cada vez
+	que el total de poblaci√≥n sea inferior al doble de la cantidad
+   inicial de poblaci√≥n, siendo as√≠ el n√∫mero de a√±os que deben pasar
+   para doblar dicha poblaci√≥n el n√∫mero de veces que se repite
    el bucle.
 	*/
 	
